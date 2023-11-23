4c3020fe56bbf9f899f5e01ce0d7b4d57244d9a9
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2023-11-23 15:48:15 +0000

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,21 @@
+//Basic Math Functions
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(7, 8));
+console.log(subtract(7, 8));
+console.log(multiply(7, 8));
+console.log(divide(7, 8));
