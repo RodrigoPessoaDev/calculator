b26da7c3a94815e66a53562134afa9d7aa3bb305
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2023-12-05 19:40:11 +0000

    Create function to operate on display expression and tweak some functions

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //Variables
 const display = document.querySelector(".textDisplay");
+const expressionDisplay = document.querySelector(".expression");
+const resultFinal = document.querySelector(".resultDisplay");
 let firstNum;
 let secondNum;
-let operator;
+let equation;
 let displayValue;
+let expressionValue;
 
 // Functions
 function add(a, b) {
@@ -36,13 +39,57 @@ function operate(a, op, b) {
 
 function updateDisplay() {
   display.textContent = displayValue;
+  expressionDisplay.textContent += displayValue;
+  equation = expressionDisplay.textContent;
+  resultFinal.textContent;
 }
 
 function clearDisplay() {
   display.textContent = "";
+  expressionDisplay.textContent = "";
 }
 
-const btn = document.querySelectorAll(".btn").forEach((element) => {
+function clearResult() {
+  resultFinal.textContent = "";
+}
+
+function calc(equation) {
+  let terms = equation.split(/([+\-*/])/);
+
+  let numbers = terms.map(function (term) {
+    return isNaN(term) ? term : parseFloat(term);
+  });
+
+  for (let i = 1; i < numbers.length - 1; i += 2) {
+    let operator = numbers[i];
+    let operand = numbers[i + 1];
+
+    if (operator === "*") {
+      numbers[i - 1] *= operand;
+      numbers.splice(i, 2);
+      i -= 2;
+    } else if (operator === "/") {
+      numbers[i - 1] /= operand;
+      numbers.splice(i, 2);
+      i -= 2;
+    }
+  }
+
+  let result = numbers[0];
+  for (let i = 1; i < numbers.length - 1; i += 2) {
+    let operator = numbers[i];
+    let operand = numbers[i + 1];
+
+    if (operator === "+") {
+      result += operand;
+    } else if (operator === "-") {
+      result -= operand;
+    }
+  }
+  return result;
+}
+
+const btn = document.querySelectorAll(".btn, .btnEqual").forEach((element) => {
   element.addEventListener("mousedown", (e) => {
     element.style.cssText += "background:grey";
   });
@@ -56,17 +103,19 @@ const btnClick = document.querySelectorAll(".btn").forEach((element) => {
     displayValue = element.innerHTML;
 
     updateDisplay();
+    clearResult();
   });
 });
+
+const btnEqual = document
+  .querySelector(".btnEqual")
+  .addEventListener("click", (s) => {
+    resultFinal.textContent = calc(equation);
+    console.log(calc(equation));
+    updateDisplay();
+    clearDisplay();
+  });
 
 document.querySelector(".btnAc").addEventListener("click", (e) => {
   clearDisplay();
 });
-
-/*const btnClick = document.querySelectorAll(".btn").forEach((element) => {
-  element.addEventListener("click", (e) => {
-    displayValue = element.innerHTML;
-
-    updateDisplay();
-  });
-}); */
